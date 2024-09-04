22ba02e82b2b8d9d26cba7a5353e0818be877fe4
NUTella <128215731+NUTellaVING@users.noreply.github.com>
2024-09-04 20:38:55 +0700

    Update BloxFruits.lua

--- Games/BloxFruits.lua
+++ Games/BloxFruits.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/cc79c5ff728749af951315180d044e2c.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/130c9dac6cc4c49688b85de6822b53e4.lua"))()
